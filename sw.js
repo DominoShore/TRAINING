68f6401064a44e6ea80d5e487fe640663d66c918
DominoShore <seandecosta2@gmail.com>
2026-09-04 19:40:37 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "train-fuel-v2";
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first for the app shell: always fetch the latest version when online
+// (and keep the cache updated in the background), only falling back to the cache
+// when there's no connection. This means future file updates show up immediately
+// on next load, instead of requiring a service-worker version bump every time.
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  const isAppShell = url.origin === self.location.origin;
+
+  if (isAppShell) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
+  // Cross-origin requests (like the food search API) are left to the network as normal.
+});
